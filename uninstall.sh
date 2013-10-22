cb8fe5a669f379bfa5e8136bb03a5783e01e1698
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2013-10-23 00:27:21 +0200

    improved, yet not finished, uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env sh
 
-echo "# vimpeppers uninstaller"
+echo "\033[1;34m# vimpeppers uninstaller\033[0m"
 
 if [ -d "$HOME/.vimpeppers" ]; then
+
 	echo "\033[1;32m✓ removing directories...\033[0m"
-	rm -rf "$HOME/.vimpeppers"
+
+  rm -rf "$HOME/.vimpeppers"
+
+  rm -rf "$HOME/.vim/bundle/*"
+
 else
-	echo "\033[1;31m! could not find directories... \033[0m"
+
+  echo "\033[1;31m✗ unable to locate folders...\033[0m"
+
 fi
 
 if [ -h "$HOME/.vimrc" ]; then
+
 	echo "\033[1;32m✓ removing symlinks...\033[0m"
-	rm "$HOME/.vimrc"
+
+	rm -f "$HOME/.vimrc"
+
 else
-	echo "\033[1;31m! could not find symlinks... \033[0m"
+
+  echo "\033[1;31m✗ unable to locate symmlinks...\033[0m"
+
 fi
 
 echo "\033[1;32m= done\033[0m"
